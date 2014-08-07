9750b86218792c5d070d4c567a471fc7da9087a8
ChongWeiSu <tuba128@msn.com>
2014-08-07 10:20:44 +0800

    add pre-lab

---
+++ pre-lab-1/fibseq.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//extern int fibonacci(int x);
+int fibonacci(int x)
+{
+	if(x==0) return 0;
+	if(x==1) return 1;
+	return fibonacci(x-1) + fibonacci(x-2);
+}
+
+int main(int argc, char **argv)
+{
+  int number=0;
+  int result=0;
+
+  scanf("%d",&number);
+  result = fibonacci(number);   
+  printf("The fibonacci sequence at %d is: %d\n", number, result);
+}
+
---
+++ pre-lab-2/mutex.s
@@ -0,0 +1,38 @@
+	.syntax unified
+	.arch armv7-a
+	.text
+
+	.equ locked, 1
+	.equ unlocked, 0
+
+	.global lock_mutex
+	.type lock_mutex, function
+lock_mutex:
+    @ INSERT CODE BELOW
+        
+    ldr r1, =locked			@ Load the 'locked taken' value, and put into r1
+	.L1:
+	ldrex r2, [r0]			@ Load the lock value from r0 (Lock Address), and put into r2
+	cmp r2, unlocked		@ Compare the 'unlocked taken' value
+	strexeq r2, r1, [r0]	@ If R2 = unlocked, store r1 into r0.
+	cmpeq r2, #0 			@ If R2 = unlocked, compre the r2 (return value of strexeq) with zero
+	bne .L1					@ If strexeq is failed or Lock Address is locked, goto .L1
+
+        @ END CODE INSERT
+	bx lr
+
+	.size lock_mutex, .-lock_mutex
+
+	.global unlock_mutex
+	.type unlock_mutex, function
+unlock_mutex:
+	@ INSERT CODE BELOW
+    
+    ldr r1, =unlocked	@ Load the 'unlocked taken' value, and put into r1
+	str r1, [r0]	@ Store r1 into r0 (Lock Address)
+
+    @ END CODE INSERT
+	bx lr
+	.size unlock_mutex, .-unlock_mutex
+
+	.end
